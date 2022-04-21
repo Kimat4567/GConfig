3d90471cc4b3f3fd7d3617aad16637f24187836f
Julian Herrera <juliann4567@gmail.com>
2022-04-21 12:58:38 -0500

    Fix myapp.py Missing modules

--- myapp.py
+++ myapp.py
@@ -1,10 +1,18 @@
+"""
+Modulo donde retornamos un hola mundo como string.
+"""
+
 import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/hi")
+
 def say_hi():
+    """
+    Retorna un string
+    """
     return 'Hola soy Alexis'
 
 if __name__=='__main__':
